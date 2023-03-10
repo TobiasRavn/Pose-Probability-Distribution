4ff3fe090e3814015d4997b74240b111dfedf949
Tobias Ravn <jacobsen.tobias@gmail.com>
2023-03-10 11:14:46 +0100

    Removed test.py file

--- blenderproc/test.py
+++
@@ -1,9 +0,0 @@
-import h5py
-import numpy as np
-import json
-
-with h5py.File("blenderproc/data/0.hdf5") as f:
-    colors = np.array(f["colors"])
-    text = np.array(f["ground_truth"]).tostring()
-ground_truth = json.loads(text)
-print(ground_truth)
