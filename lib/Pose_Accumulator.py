a52d6a3f2fd9b8c48f6a076d669e5a4f2cddd4e6
mahut17 <mahut17@student.sdu.dk>
2023-03-24 18:54:15 +0100

    Added main Sample space & Pose Accumulator initial version done

--- lib/Pose_Accumulator.py
+++ lib/Pose_Accumulator.py
@@ -1,35 +1,48 @@
 
 import math
 
+import numpy as np
 
 
 class Pose_Accumulator:
 
-    def __init__(self, step_x, x_min, x_max, step_y, y_min, y_max, step_r):
-        self._index=0
-        self.allPoses=[]
+    def __init__(self, step_x, x_min, x_max, step_y, y_min, y_max, step_r,training=False):
+        self._index=-1
+        #self.allPoses=[]
         self.allResults=[]
         self.count=0
-        for x in range(x_min,x_max,step_x):
-            for y in range(y_min, y_max, step_y):
-                for r in range(0, 360, step_r):
+        x_num=round((x_max-x_min+step_x)/step_x)
+        y_num = round((y_max - y_min + step_y) / step_y)
+        r_num = round((360) / step_r)
+        x_range=np.linspace(x_min,x_max,int(x_num))
+        y_range = np.linspace(y_min, x_max, int(y_num))
+        r_range = np.linspace(0, 360-step_r, int(r_num))
+
+        self.size=x_num*y_num*r_num
+        self.allPoses=np.zeros([self.size,4])
+
+        print(r_range)
+        for x in x_range:
+            for y in y_range:
+                for r in r_range:
                     r_rad=math.radians(r)
                     self.allResults.append(0.0)
-                    self.count+=1
-                    self.allPoses.append([x,y,math.cos(r_rad),math.sin(r_rad)])
 
+                    self.allPoses[self.count]=np.array([x,y,math.cos(r_rad),math.sin(r_rad)])
+                    self.count += 1
         pass
 
     def __iter__(self):
-        return self.allPoses[self._index]
+        #return self.allPoses[self._index]
         return self
 
     def result(self,result):
         self.allResults[self._index]=result
-
+    def pose(self):
+        return self.allPoses[self._index]
     def __next__(self):
         self._index+=1
         if(self._index>=self.count):
             raise StopIteration
 
-
+        return self.allPoses[self._index]
---
+++ lib/main.py
@@ -0,0 +1,47 @@
+
+
+import sys
+import os
+import glob
+import numpy as np
+
+
+from Controller import *
+from load_image import *
+
+
+
+
+def main() -> int:
+    """Echo the input arguments to standard output"""
+
+    dir = "../blenderproc/data"
+    training = True
+
+
+    files=glob.glob(dir+"/*.hdf5")
+
+    image, ground_truth = load_image(files[0])
+
+    img=np.array(image)
+    size=(np.prod(img.shape))
+    controller = Controller(size)
+    x_step=0.1
+    y_step=0.1
+    r_step=4
+
+    for file in files:
+        image, ground_truth = load_image(file)
+        controller.sample_space(image,x_step,y_step,r_step,truth=ground_truth,training=training)
+
+
+
+
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
+
+
+
