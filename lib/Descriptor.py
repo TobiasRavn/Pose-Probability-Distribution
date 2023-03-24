b56d8789b8b6df505eb0f1f24812509c4ec68c0f
SteffenWJ <steffenwaagejensen@gmail.com>
2023-03-24 13:52:17 +0100

    Added PIL support in discriptor

--- lib/Descriptor.py
+++ lib/Descriptor.py
@@ -28,7 +28,7 @@ class Descriptor:
         return self.length_of_visual_description
     
     def __init__(self, image_size, model_weights = "imagenet"):
-        self.base_descriptor_model = tf.keras.applications.ResNet101V2(weights=model_weights,
+        self.base_descriptor_model = tf.keras.applications.ResNet50V2(weights=model_weights,
                                                include_top=False,               
                                                input_shape=image_size)
         
@@ -38,62 +38,3 @@ class Descriptor:
         
         self.length_of_visual_description = layers_added.shape[-1]
         self.vision_model = tf.keras.Model(self.input_image_size, layers_added)
-
-class tranining_model:
-    def show_MLP_model(self):
-        self.implicit_model.summary()
-        
-    def return_model(self):
-        return self.implicit_model
-    
-    
-    def __init__(self, lenght_visual_description,MLP_layer_size, number_fourier_components = 0) -> None:
-        self.len_visual_description = lenght_visual_description
-        #self.len_rotation = 9 #Orignal
-        self.len_rotation = 3
-        self.fourier = number_fourier_components
-        
-        if number_fourier_components == 0:
-            self.length_query = self.len_rotation
-        else:
-            self.length_query = self.len_rotation * self.fourier
-        
-        
-        input_visual = tf_keras_layers.Input(shape=(lenght_visual_description))
-        visual_embedding = tf_keras_layers.Dense(MLP_layer_size[0])(input_visual)
-        input_query = tf_keras_layers.Input(shape=(None, self.length_query,))
-        query_embedding = tf_keras_layers.Dense(MLP_layer_size[0])(input_query)
-        
-        output = visual_embedding[:, tf.newaxis] + query_embedding
-        
-        output = tf_keras_layers.ReLU()(output)
-        
-        for num_units in MLP_layer_size[1:]:
-            output = tf_keras_layers.Dense(num_units, 'relu')(output)
-        output = tf_keras_layers.Dense(1)(output)  #Need to look into why it works
-        self.implicit_model = tf.keras.models.Model(
-            inputs=[input_visual, input_query],
-            outputs=output)
-        self.MPL_layer_size = MLP_layer_size
-        
-        
-
-
-desc_test = Descriptor((255,255,3))
-
-desc_test.show_base_model()
-desc_test.show_vision_model()
-
-disc_output = desc_test.get_image_descriptor("lib/test_images/png_dog_smol.png")
-disc_vis_ou = desc_test.get_length_of_visual_description()
-print(disc_output)
-print(disc_vis_ou)
-
-train_test = tranining_model(disc_vis_ou,[255]*2)
-
-
-train_test.show_MLP_model()
-
-
-the_train_model = train_test.return_model()
-
