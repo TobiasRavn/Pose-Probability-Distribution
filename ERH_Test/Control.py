ae55fa21aff1fa2e3b449b7932eabfa79439c4d4
reventlov <erh@audite.ai>
2023-04-28 19:00:33 +0200

    ERH

--- ERH_Test/Control.py
+++ ERH_Test/Control.py
@@ -81,8 +81,6 @@ class PoseEstimationDataset(keras.utils.Sequence):
         # Generate descriptors
         X_descriptors = []
         for image in X:
-            # Remove the line below
-            #image_descriptor = Descriptor(image_size=(1000, 1000, 3))
             image_descriptor_array = self.image_descriptor.get_image_descriptor_array(image)
             X_descriptors.append(image_descriptor_array)
 
@@ -102,221 +100,67 @@ class PoseEstimationDataset(keras.utils.Sequence):
             X[i,] = self.images[index]
 
             # Store ground truth
-            y[i,] = self.ground_truths[index]
+            y[i,] = self.ground_truths[index
+            # Apply random horizontal flip with probability p_flip
+            if self.p_flip and np.random.rand() < self.p_flip:
+                X[i,] = np.fliplr(X[i,])
+                y[i,] = np.fliplr(y[i,])
+
+            # Apply random rotation with probability p_rot
+            if self.p_rot and np.random.rand() < self.p_rot:
+                angle = np.random.uniform(-self.max_rot_angle, self.max_rot_angle)
+                X[i,] = rotate(X[i,], angle, mode='reflect')
+                y[i,] = rotate(y[i,], angle, mode='reflect')
+
+            # Apply random zoom with probability p_zoom
+            if self.p_zoom and np.random.rand() < self.p_zoom:
+                zoom_factor = np.random.uniform(1 - self.max_zoom, 1 + self.max_zoom)
+                X[i,] = zoom(X[i,], zoom_factor, mode='reflect')
+                y[i,] = zoom(y[i,], zoom_factor, mode='reflect')
 
         return X, y
-
-class MLP:
-    def __init__(self, descriptor_shape):
-        self.model = Sequential([
-            Dense(256, activation='relu', input_shape=descriptor_shape, name='dense_1'),  # Modify the input shape
-            Dense(256, activation='relu', name='dense_2'),
-            Dense(3, activation='softmax', name='predictions')
-        ])
-
-        self.descriptor_shape = descriptor_shape
-        #self.lr_schedule = 1e-7
-
-        # Learning rate scheduling
-        self.lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate=1e-7, # Set your desired initial learning rate
-            decay_steps=10000,
-            decay_rate=0.9)
-
-        # Define optimizer and loss function
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr_schedule)
-        self.model.compile(optimizer=self.optimizer, loss='mse')
-
-def plotHeatmap(predictions, ground_truths):
-
-    print("Predictions:", predictions)
-    print("Ground Truths:", ground_truths)
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111, projection='3d')
-
-    for i, (x_pred, y_pred, r_pred) in enumerate(predictions):
-        # Add predicted pose as a red dot
-        ax.plot([x_pred], [y_pred], [r_pred], marker='o', markersize=10, color="red")#, label='PP')
-
-        # Add ground truth as a blue dot
-        gt_x, gt_y, gt_z = map(float, ground_truths[i])
-        ax.plot([gt_x], [gt_y], [gt_z], marker='o', markersize=10, color="blue")#, label='GT')
-
-    # Set axis labels and titles
-    ax.set_xlabel("X-coor")
-    ax.set_ylabel("Y-coor")
-    ax.set_zlabel("Z-coor")
-    ax.set_title("Predicted and Ground Truth Poses", fontsize=20)
-    ax.legend()
-
-     # Calculate the average distance between predicted poses and ground truth poses
-    distances = []
-    for i, (x_pred, y_pred, r_pred) in enumerate(predictions):
-        gt_x, gt_y, gt_z = map(float, ground_truths[i])
-        distance = np.sqrt((x_pred - gt_x)**2 + (y_pred - gt_y)**2 + (r_pred - gt_z)**2)
-        distances.append(distance)
-
-    average_distance = np.mean(distances)
-
-    # Create a unique filename for the plot
-    filename = f"heatmap_{len(ground_truths)}.png"
-    # Save the plot as a PNG image in the 'plots' folder
-    plt.savefig(f'/Users/reventlov/Documents/Robcand/2. Semester/ProjectARC/Project/Implicit-PDF/ERH_Test/plot/{filename}')
-    #plt.close(fig)
-    plt.show()
-
-    return average_distance
-
-def predict_poses(model, image_arrays, image_descriptor):
-    outputs = []
-    for image_array in image_arrays:
-        # Get the image descriptor 
-        image_descriptor_array = image_descriptor.get_image_descriptor_array(image_array)
-        print("image_descriptor_array: ", image_descriptor_array)
-        #input("Press any key to continue...")
-
-        # Add a batch dimension to the image descriptor array
-        image_descriptor_array = np.expand_dims(image_descriptor_array, axis=0)
-
-        # Use the image descriptor as input to the model
-        output = model.model.predict(image_descriptor_array)
         
-        # Remove extra dimensions
-        output = np.squeeze(output)
-
-        x, y, r = output
-        outputs.append((x, y, r))
-        #print(f"Predicted pose: ({x}, {y}, {r})")
-        print("outputs: ", outputs)
-        #input("Press any key to continue...")
-    return outputs
-
-def load_image(path):
-    f = h5py.File(path, 'r')
-    image = f.get('colors')[()]
-    byte_str = f.get('ground_truth')[()]
-    dict_str = byte_str.decode("UTF-8")
-    ground_truth_dict = ast.literal_eval(dict_str)
-    ground_truth = [ground_truth_dict['x'], ground_truth_dict['y'], ground_truth_dict['r']]
-    f.close()
-    return image, ground_truth
-
-def main():
+class AugmentedPoseEstimationDataset(PoseEstimationDataset):
+    def init(self, images, ground_truths, image_descriptor, batch_size=2, shuffle=True,
+    p_flip=0.5, p_rot=0.5, max_rot_angle=10, p_zoom=0.5, max_zoom=0.1):
+    super().init(images, ground_truths, image_descriptor, batch_size=batch_size, shuffle=shuffle)
+    self.p_flip = p_flip
+    self.p_rot = p_rot
+    self.max_rot_angle = max_rot_angle
+    self.p_zoom = p_zoom
+    self.max_zoom = max_zoom
     
-    print(f"Python Platform: {platform.platform()}")  
-    gpu = len(tf.config.list_physical_devices('GPU'))>0
-    print("GPU is", "available" if gpu else "NOT AVAILABLE")
-   
-    device = "/gpu:0" if tf.config.list_physical_devices('GPU') else "/cpu:0"
-    
-    # Print the selected device
-    print(f"Running on {device}")
+    def __data_generation(self, indexes):
+    """
+    Generates data containing batch_size samples with augmentations.
+    """
+    # Initialization
+    X = np.empty((self.batch_size, *self.images.shape[1:]))
+    y = np.empty((self.batch_size, *self.ground_truths.shape[1:]))
 
-    # Rest of your code inside the context manager
-    with tf.device('/device:GPU:0'):
-        dir = "/Users/reventlov/Documents/Robcand/2. Semester/ProjectARC/Project/IPDF/data1000"
-        files = glob.glob(dir + "/*.hdf5")
-        image_arrays = []
-        ground_truths = []
-        
-        
-        for i in range(0, len(files), 80): # only load every 40 number of file
-            file = files[i]
-            image, ground_truth = load_image(file)
-            image_arrays.append(image)
-            ground_truths.append(ground_truth)
+    # Generate data
+    for i, index in enumerate(indexes):
+        # Store sample
+        X[i,] = self.images[index]
 
-        image_arrays = np.array(image_arrays)
-        ground_truths = np.array(ground_truths)
+        # Store ground truth
+        y[i,] = self.ground_truths[index]
 
-        # Split the dataset into training and validation sets
-        train_image_arrays, val_image_arrays, train_ground_truths, val_ground_truths = train_test_split(image_arrays, ground_truths, test_size=0.1, random_state=42)
+        # Apply random horizontal flip with probability p_flip
+        if self.p_flip and np.random.rand() < self.p_flip:
+            X[i,] = np.fliplr(X[i,])
+            y[i,] = np.fliplr(y[i,])
 
-        image_descriptor = Descriptor(image_size=(1000, 1000, 3))
+        # Apply random rotation with probability p_rot
+        if self.p_rot and np.random.rand() < self.p_rot:
+            angle = np.random.uniform(-self.max_rot_angle, self.max_rot_angle)
+            X[i,] = rotate(X[i,], angle, mode='reflect')
+            y[i,] = rotate(y[i,], angle, mode='reflect')
 
-        print(f"Number of training images: {len(train_image_arrays)}")
-        print(f"Number of validation images: {len(val_image_arrays)}")
+        # Apply random zoom with probability p_zoom
+        if self.p_zoom and np.random.rand() < self.p_zoom:
+            zoom_factor = np.random.uniform(1 - self.max_zoom, 1 + self.max_zoom)
+            X[i,] = zoom(X[i,], zoom_factor, mode='reflect')
+            y[i,] = zoom(y[i,], zoom_factor, mode='reflect')
 
-        input("Press any key to continue...")
-
-        descriptor_shape = (1, image_descriptor.get_length_of_visual_description())
-
-        # Prepare the dataset
-        train_dataset = PoseEstimationDataset(train_image_arrays, train_ground_truths, image_descriptor)    
-        val_dataset = PoseEstimationDataset(val_image_arrays, val_ground_truths, image_descriptor)
-
-        model = MLP(descriptor_shape)
-        #model = MLP(descriptor_shape, image_descriptor)
-
-
-        model.model.summary()
-
-        # Print the descriptor shape
-        print("Descriptor shape:", descriptor_shape)
-
-        num_epochs = 5
-        # Initialize a list to store the average loss for each epoch
-        losses = []
-        
-        for epoch in range(num_epochs):
-            history = model.model.fit(train_dataset, epochs=1, validation_data=val_dataset)  # Change the epochs to 1
-
-            # Collect the training loss for the current epoch
-            loss = history.history['loss'][0]
-            losses.append(loss)
-            print(f"Epoch: {epoch+1}, Loss: {loss:.4f}")
-
-    # Save the model
-        model.model.save("pose_estimation_model.h5")
-
-        predictions = predict_poses(model, val_image_arrays, image_descriptor)
-
-        # Predict the poses for validation images
-        predictions = [(x[0], x[1],  x[2]) for x in predictions]
-
-        # Initialize a list to store the average distances
-        average_distances = []
-        # Load different numbers of data points and plot the heatmaps
-        num_data_points_range = range(10, len(image_arrays) + 1, 10)
-        for num_data_points in num_data_points_range:
-            # Select a subset of data points
-            subset_image_arrays = image_arrays[:num_data_points]
-            subset_ground_truths = ground_truths[:num_data_points]
-
-            # Predict the poses for the subset of images
-            predictions = predict_poses(model, subset_image_arrays, image_descriptor)
-            predictions = [(x[0], x[1], x[2]) for x in predictions]
-
-            # Plot heatmap with predicted and ground truth coordinates and calculate the average distance
-            average_distance = plotHeatmap(predictions, subset_ground_truths.tolist())
-            average_distances.append(average_distance)
-
-        # Plot the relationship between the number of data points and the average distance
-        plt.plot(num_data_points_range, average_distances, marker='o', linestyle='-', markersize=6)
-        plt.xlabel('Number of Data Points')
-        plt.ylabel('Average Distance')
-        plt.title('Average Distance vs. Number of Data Points')
-
-        # Save the plot as a PNG image in the 'plots' folder
-        filename1 = f"average_distance_vs_num_data_points.png"
-        plt.savefig(f'/Users/reventlov/Documents/Robcand/2. Semester/ProjectARC/Project/Implicit-PDF/ERH_Test/plot/{filename1}')
-        plt.close()
-
-        # Create a list of epoch numbers
-        epochs = list(range(1, len(losses) + 1))
-
-        # Create the MSE-loss plot
-        plt.plot(epochs, losses, marker='o', linestyle='-', markersize=6)
-        plt.xlabel('Epochs')
-        plt.ylabel('MSE Loss')
-        plt.title('Loss vs. Epochs')
-
-        # Save the plot as a PNG image in the 'plots' folder
-        filename2 = f"mse_Epoc{num_epochs}_Loss{len(train_image_arrays) + len(val_image_arrays)}.png"
-
-        # Save the plot as a PNG image in the 'plots' folder
-        plt.savefig(f'/Users/reventlov/Documents/Robcand/2. Semester/ProjectARC/Project/Implicit-PDF/ERH_Test/plot/{filename2}')
-        plt.close()
-
-if __name__ == "__main__":
-    main()
+    return X, y
