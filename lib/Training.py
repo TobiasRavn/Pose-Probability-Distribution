2ce2400df51f761d34ddad1164a01a50c513f8c0
mahut17 <mahut17@student.sdu.dk>
2023-05-05 14:24:32 +0200

    Merge Visual

--- lib/Training.py
+++ lib/Training.py
@@ -1,30 +1,28 @@
-
 import random
 
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import glob
+
 tfkl = tf.keras.layers
 import matplotlib.pyplot as plt
 import time
-#own libs
+# own libs
 from lib.load_image import *
 from lib.Descriptor import *
 from lib.Poses import *
 
 from lib.ModelArchitecture import *
+from lib.Visual import *
 
 
 class Training:
     def __init__(self, dir, doEvaluation=True):
 
-
-        self.doEvaluation=doEvaluation
+        self.doEvaluation = doEvaluation
         self.dir = dir
         files = glob.glob(self.dir + "/*.hdf5")
-
-
 
         random.shuffle(files)
         # split files into training and validation data
@@ -35,7 +33,7 @@ class Training:
         self.current_test_name = "batch_4_epoch_100_1e_4_triangle"
 
         # load first image to use img size
-        image, ground_truth = load_image(files[0])
+        image, self.ground_truth = load_image(files[0])
         img = np.array(image)
         self.imgSize = img.shape
         # init descriptor
@@ -50,36 +48,35 @@ class Training:
         # define optimizer
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
-
-
         self.x_min, self.x_max = -0.3, 0.3
         self.y_min, self.y_max = -0.3, 0.3
         self.r_min, self.r_max = 0, 360
 
         self.position_samples = 100
-        loss_list = []
-        validation_loss_list = []
         self.epochs = 100
         self.batch_size = 4
 
-        self.modelAchitecture=ModelArchitecture(self.lenDiscriptors,self.lenPose, self.imgSize)
+        self.modelAchitecture = ModelArchitecture(self.lenDiscriptors, self.lenPose, self.imgSize)
 
+        self.prediction = None
+        self.loss_list = []
+        self.vali_loss = []
+        self.epoch_loss = []
+        self.validation_loss_epoch = []
 
-    @tf.function
+        self.debug_loss = Plot_loss()
+
     def compute_loss(self, images, poses, training=True):
 
-        logits_norm = self.modelAchitecture.generate_pdf(images,poses,training)
-
+        logits_norm = self.modelAchitecture.generate_pdf(images, poses, training)
+        print(f"Logifts norm: {logits_norm[:, -1]}")
         loss_value = -tf.reduce_mean(tf.math.log(logits_norm[:, -1] / (
-                    ((0.6 ** 2) * 3.1415 * 2) / poses.shape[1])))  # index -1 because last one is the correct pose
+                ((0.6 ** 2) * 3.1415 * 2) / poses.shape[1])))  # index -1 because last one is the correct pose
         return loss_value
-
-
 
     @tf.function
     def train_step(self, optimizer, images, poses):
         with tf.GradientTape() as tape:
-
             loss = self.compute_loss(images, poses)
         grads = tape.gradient(
             loss,
@@ -90,25 +87,19 @@ class Training:
         return loss
 
     def validation_step(self, images, poses):
-        #vision_description = self.modelAchitecture.descriptor.vision_model(images, training=False)
+        # vision_description = self.modelAchitecture.descriptor.vision_model(images, training=False)
         loss = self.compute_loss(images, poses, training=False)
 
         return loss
 
-
-
     def evaluate(self, images):
-        poses=self.get_all_poses(100,100,100)
+        poses = self.get_all_poses(100, 100, 100)
 
-        logLikelihood = -self.compute_loss(images,poses,False)
+        logLikelihood = -self.compute_loss(images, poses, False)
 
         return logLikelihood
 
-
-
-
-
-    def epochTrain(self, files):
+    def epochTrain(self, files, debug=False):
         temp_epoch_loss = []
 
         #    file = random.sample(vali_data, 1)
@@ -128,22 +119,26 @@ class Training:
             # init images
             images = np.zeros((len(batch), self.imgSize[0], self.imgSize[1], self.imgSize[2]))
             # init ground truth
-            ground_truths = np.zeros((len(batch), self.position_samples, 4))
+            poses = np.zeros((len(batch), self.position_samples, 4))
             for i, file in enumerate(batch):
                 image, ground_truth = load_image(file)
                 images[i] = image
 
-                ground_truths[i] = get_random_poses_plus_correct(self.position_samples, ground_truth)
+                poses[i] = get_random_poses_plus_correct(self.position_samples, ground_truth)
 
             # convert numpy arrays to tensors
             images = tf.convert_to_tensor(images)
-            ground_truths = tf.convert_to_tensor(ground_truths)
+            poses = tf.convert_to_tensor(poses)
 
             # time to train_step
             st = time.time()
-            loss = self.train_step(self.optimizer, images, ground_truths)
+            loss = self.train_step(self.optimizer, images, poses)
+            self.loss_list.append(loss)
+            temp_epoch_loss.append(loss)
+            if debug:
+                self.debug_loss(self.loss_list)
             print("loss: ", loss.numpy(), " time: ", time.time() - st)
-
+        self.epoch_loss.append(np.mean(temp_epoch_loss))
 
     def epochEval(self, files):
 
@@ -151,8 +146,7 @@ class Training:
         random.shuffle(validations_set)
 
         batches = [validations_set[x:x + self.batch_size] for x in range(0, len(validations_set), self.batch_size)]
-
-        validation_loses = []
+        temp_validation_loses = []
 
         for count, batch in enumerate(batches):
             images = np.zeros((len(batch), self.imgSize[0], self.imgSize[1], self.imgSize[2]))
@@ -174,47 +168,51 @@ class Training:
             loss = self.validation_step(images, ground_truths)
             # print("loss: ", loss.numpy(), " time: ", time.time() - st)
 
-            validation_loses.append(loss)
-
+            temp_validation_loses.append(loss)
+            self.vali_loss.append(loss)
+        self.validation_loss_epoch.append(np.mean(temp_validation_loses))
         file = random.sample(self.vali_data, 1)
 
-        image, ground_truth = load_image(file[0])
+        image, self.ground_truth = load_image(file[0])
         images = [image]
         images = tf.convert_to_tensor(images)
-        all_poses = get_all_poses(25, 25, 25)
-        predictions = self.modelAchitecture.generate_pdf(images, all_poses)
-        #plotHeatmap(all_poses, predictions, ground_truth, heat_fig, heat_ax, epoch_counter)
+        self.all_poses = get_all_poses(25, 25, 25)
+        self.predictions = self.modelAchitecture.generate_pdf(images, self.all_poses)
+        # plotHeatmap(all_poses, predictions, ground_truth, heat_fig, heat_ax, epoch_counter)
 
-        #print("Epoch loss: ", np.mean(np.array(temp_epoch_loss)))
-        #epoch_lose_list.append(np.mean(np.array(temp_epoch_loss)))
-        #ax_loss_epoch.clear()
-        #ax_loss_epoch.plot(epoch_lose_list)
-        #ax_loss_epoch.plot(validation_loss_list, 'bo')
-        #ax_loss_epoch.set_xlabel("Epoch")
-        #ax_loss_epoch.set_ylabel("Loss")
-        #ax_loss_epoch.set_title("Loss over Epoch", fontsize=20)
-        #fig_loss_epoch.canvas.draw()
-        #fig_loss_epoch.canvas.flush_events()
-#
-        #mlp_model.save("mlp_" + current_test_name + "_" + str(epoch_counter))
-        #descriptor.vision_model.save("vm_" + current_test_name + "_" + str(epoch_counter))
-        #heat_fig.savefig("new_training_output/Heat_map_" + current_test_name + "_" + str(epoch_counter) + ".png")
-        #fig.savefig("new_training_output/loss_" + current_test_name + "_" + str(epoch_counter) + ".png")
-        #epoch_counter += 1
-        #validation_loss = np.mean(np.array(validation_loses))
-        #print("Validation loss: ", validation_loss)
-        #validation_loss_list.append(validation_loss)
-        #
+        # print("Epoch loss: ", np.mean(np.array(temp_epoch_loss)))
+        # epoch_lose_list.append(np.mean(np.array(temp_epoch_loss)))
+        # ax_loss_epoch.clear()
+        # ax_loss_epoch.plot(epoch_lose_list)
+        # ax_loss_epoch.plot(validation_loss_list, 'bo')
+        # ax_loss_epoch.set_xlabel("Epoch")
+        # ax_loss_epoch.set_ylabel("Loss")
+        # ax_loss_epoch.set_title("Loss over Epoch", fontsize=20)
+        # fig_loss_epoch.canvas.draw()
+        # fig_loss_epoch.canvas.flush_events()
+
+    #
+    # mlp_model.save("mlp_" + current_test_name + "_" + str(epoch_counter))
+    # descriptor.vision_model.save("vm_" + current_test_name + "_" + str(epoch_counter))
+    # heat_fig.savefig("new_training_output/Heat_map_" + current_test_name + "_" + str(epoch_counter) + ".png")
+    # fig.savefig("new_training_output/loss_" + current_test_name + "_" + str(epoch_counter) + ".png")
+    # epoch_counter += 1
+    # validation_loss = np.mean(np.array(validation_loses))
+    # print("Validation loss: ", validation_loss)
+    # validation_loss_list.append(validation_loss)
+    #
 
     def startTraining(self, epochs):
-
         print("Starting training")
         epoch_lose_list = []
+        heat_map = Heat_map(self.modelAchitecture)
+        plot_loss = Plot_loss()
+
         for epoch in range(epochs):
-            self.epochTrain(self.train_data)
-
+            self.epochTrain(self.train_data, True)
             self.epochEval(self.vali_data)
-
+            # plot_loss(self.epoch_loss,self.validation_loss_epoch)
+            heat_map(self.vali_data[0])
             # Validation
-
+        plot_loss.save_figure("output/loss.png")
 
--- lib/Visual.py
+++ lib/Visual.py
@@ -1,110 +1,87 @@
-
 import random
 import math
+from typing import Any
 
 import numpy as np
 import tensorflow as tf
 import glob
 import matplotlib.pyplot as plt
 import time
-#own libs
+# own libs
 from lib.load_image import *
+from lib.ModelArchitecture import *
+from lib.Poses import *
 
 
+class Heat_map:
+    def save_figure(self, path):
+        self.heat_figure.savefig(path)
 
-class Visual:
-    def __init__(self):
-        pass
+    def __init__(self, model, figsize=(12, 6)):
+        self.model = model
+        self.heat_figure = plt.figure(figsize=figsize)
+        self.image_ax = self.heat_figure.add_subplot(122)
+        self.heat_ax = self.heat_figure.add_subplot(121, projection='3d')
 
-        #init plot of loss using plt
-        plt.ion()
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-
-
-        fig_loss_epoch = plt.figure()
-        ax_loss_epoch = fig_loss_epoch.add_subplot(111)
-
-        # loss_list.append(loss.numpy())
-        # temp_epoch_loss.append(loss.numpy())
-        # ax.clear()
-        # ax.plot(loss_list)
-        # ax.set_xlabel("Batch")
-        # ax.set_ylabel("Loss")
-        # ax.set_title("Loss per Batch", fontsize=20)
-        # fig.canvas.draw()
-        # fig.canvas.flush_events()
-
-
-
-
-
-    def plotLoss(self, losses):
-        pass
-
-
-    def plotHeatmap(self, poses, predictions, ground_truth, epoch_counter, figName="heatmap"):
-        # Construct covariance matrix
-
-        self.tryCreatePlot(figName)
-
-        # plt.clf()
+    def __call__(self, path):
+        self.heat_figure.show()
+        image, ground_truth = load_image(path)
+        images = [image]
+        images = tf.convert_to_tensor(images)
+        all_poses = get_all_poses(25, 25, 25)
+        predictions = self.model.generate_pdf(images, all_poses)
         predictions = np.squeeze(predictions)
         predictions = predictions / np.max(predictions)
-
-
-
-
-        # print("Predictions:", predictions)
-        # print("Ground Truths:", ground_truth)
-        # heat_fig = plt.figure(figsize=(8, 6))
-        # heat_ax = heat_fig.add_subplot(111, projection='3d')
-        # print(np.shape(predictions))
         self.heat_ax.clear()
         for i in range(np.size(predictions)):
-            pose = poses[i]
-
+            pose = all_poses[i]
             x_pred = pose[0]
             y_pred = pose[1]
             r_pred = math.atan2(pose[3], pose[2])
             # print(predictions[i])
+            self.heat_ax.plot([x_pred], [y_pred], [r_pred], marker='o', markersize=2, color="red",
+                              alpha=np.clip(predictions[i] - 0.1, 0, 1))  # , label='PP')
 
-            heat_ax.plot([x_pred], [y_pred], [r_pred], marker='o', markersize=2, color="red",
-                         alpha=np.clip(predictions[i] - 0.1, 0, 1))  # , label='PP')
-
-        # Add ground truth as a blue dot
         gt_x = float(ground_truth["x"])
         gt_y = float(ground_truth["y"])
         gt_z = float(ground_truth["r"])
         gt_z = math.radians(gt_z)
         if gt_z > math.pi:
             gt_z = gt_z - 2 * math.pi
-        heat_ax.plot([gt_x], [gt_y], [gt_z], marker='o', markersize=10, color="blue")  # , label='GT')
+        self.image_ax.imshow(image)
+        self.image_ax.axis('off')
+        self.heat_ax.plot([gt_x], [gt_y], [gt_z], marker='o', markersize=10, color="blue")  # , label='GT')
+        self.heat_ax.set_xlabel("X-coor")
+        self.heat_ax.set_ylabel("Y-coor")
+        self.heat_ax.set_zlabel("Rotation")
+        self.heat_ax.set_title("Predicted and Ground Truth Poses", fontsize=20)
+        self.heat_ax.legend()
+        self.heat_figure.canvas.draw()
+        self.heat_figure.canvas.flush_events()
+        print("Done Plotting Heat Map")
 
-        # Set axis labels and titles
-        heat_ax.set_xlabel("X-coor")
-        heat_ax.set_ylabel("Y-coor")
-        heat_ax.set_zlabel("Z-coor")
-        heat_ax.set_title("Predicted and Ground Truth Poses", fontsize=20)
-        heat_ax.legend()
 
-        # Calculate the average distance between predicted poses and ground truth poses
-        # distances = []
-        # for i, (x_pred, y_pred, r_pred) in enumerate(predictions):
-        #    gt_x, gt_y, gt_z = map(float, ground_truths[i])
-        #    distance = np.sqrt((x_pred - gt_x) ** 2 + (y_pred - gt_y) ** 2 + (r_pred - gt_z) ** 2)
-        #    distances.append(distance)
+class Plot_loss:
+    def save_figure(self, path):
+        self.loss_figure.savefig(path)
 
-        # average_distance = np.mean(distances)
-        print("Done Plotting")
-        # Create a unique filename for the plot
-        filename = f"heatmap_{len(ground_truth)}.png"
-        # Save the plot as a PNG image in the 'plots' folder
-        # plt.show()
-        heat_fig.canvas.draw()
-        heat_fig.canvas.flush_events()
-        # plt.savefig(
-        #    f'/{filename}')
-        # plt.close(fig)
+    def __init__(self, plot_name="Loss", plot_x_name="Epoch", plot_y_name="Loss"):
+        self.name = plot_name
+        self.x_name = plot_x_name
+        self.y_name = plot_y_name
+        self.loss_figure = plt.figure()
+        self.loss_ax = self.loss_figure.add_subplot(111)
 
-        # return average_distance
+    def __call__(self, data_list_A, data_list_B=None):
+        self.loss_figure.show()
+        self.loss_ax.clear()
+        self.loss_ax.plot(data_list_A)
+        if data_list_B is not None:
+            self.loss_ax.plot(data_list_B, 'ro')
+        self.loss_ax.axhline(y=0.8161, color='0.8', linestyle='--')
+        self.loss_ax.axhline(y=-3.7889, color='0.8', linestyle='--')
+        self.loss_ax.set_xlabel(self.x_name)
+        self.loss_ax.set_ylabel(self.y_name)
+        self.loss_ax.set_title(self.name, fontsize=20)
+        self.loss_figure.canvas.draw()
+        self.loss_figure.canvas.flush_events()
